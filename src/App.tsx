d4ced9119e356df6a3933084e74723cddd0766f9
danny-veliz <106838709+danny-veliz@users.noreply.github.com>
2025-10-30 21:14:17 +0000

    Remove unused import

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
 //import viteLogo from '/vite.svg'
 import { styled } from '@mui/material/styles';
 import Box from '@mui/material/Box';
