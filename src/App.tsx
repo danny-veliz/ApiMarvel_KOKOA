3aad985ebec42fc2697c819d84240267b0025308
danny-veliz <106838709+danny-veliz@users.noreply.github.com>
2025-10-30 21:10:31 +0000

    Comment

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+//import viteLogo from '/vite.svg'
 import { styled } from '@mui/material/styles';
 import Box from '@mui/material/Box';
 // Grid version 2
